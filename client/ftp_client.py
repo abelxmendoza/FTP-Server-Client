d89c805239cd16ee80fe130ae4fe40909e6020c6
Abel Mendoza <abelxmendoza@gmail.com>
2023-11-08 17:21:41 -0800

    first push

---
+++ client/ftp_client.py
@@ -0,0 +1,37 @@
+import socket
+
+def main():
+    server_host = '127.0.0.1'  # Update with your server's IP address
+    server_port = 21  # Update with your server's port number
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((server_host, server_port))
+
+    while True:
+        response = client.recv(1024).decode()
+        print(response)
+
+        command = input("Enter a command: ")
+        client.send(command.encode())
+
+        if command == "quit":
+            break
+        elif command.startswith("get "):
+            filename = command[4:]
+            data = client.recv(1024)
+            with open(filename, "wb") as file:
+                file.write(data)
+        elif command.startswith("put "):
+            filename = command[4:]
+            with open(filename, "rb") as file:
+                data = file.read()
+                client.send(data)
+        elif command == "ls":
+            file_list = client.recv(1024).decode()
+            print(file_list)
+
+    client.close()
+
+if __name__ == '__main__':
+    main()
+
---
+++ server/ftp_server.py
@@ -0,0 +1,51 @@
+import socket
+import os
+
+# Function to list files in the server directory
+def list_files():
+    files = os.listdir('.')
+    return "\n".join(files)
+
+# Function to handle client requests
+def handle_client(client_socket):
+    welcome_message = "Welcome to the FTP server. Available commands: ls, get <filename>, put <filename>"
+    client_socket.send(welcome_message.encode())
+
+    while True:
+        request = client_socket.recv(1024).decode()
+        if not request:
+            break
+
+        if request == "ls":
+            file_list = list_files()
+            client_socket.send(file_list.encode())
+        elif request.startswith("get "):
+            filename = request[4:]
+            try:
+                with open(filename, "rb") as file:
+                    client_socket.send(file.read())
+            except FileNotFoundError:
+                client_socket.send("File not found".encode())
+        elif request.startswith("put "):
+            filename = request[4:]
+            data = client_socket.recv(1024)
+            with open(filename, "wb") as file:
+                file.write(data)
+            client_socket.send("File uploaded successfully".encode())
+        else:
+            client_socket.send("Invalid command".encode())
+
+    client_socket.close()
+
+# Create a socket and start listening for connections
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 21))
+server.listen(5)
+print("FTP server listening on port 21")
+
+while True:
+    client_socket, addr = server.accept()
+    print(f"Accepted connection from {addr}")
+    handle_client(client_socket)
+
+
