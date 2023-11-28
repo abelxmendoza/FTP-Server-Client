fee0632e683dce30890d595ce766c0439f47722e
Daniel Quezada <dquezada478@gmail.com>
2023-11-28 14:39:04 -0800

    added non-existant file check in client

--- client/ftp_client.py
+++ client/ftp_client.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import os
 
 
 def executeCommands(client):
@@ -8,7 +9,7 @@ def executeCommands(client):
 
     if command == "quit":
         return False
-    elif command.startswith("get "): # get data from server
+    elif command.startswith("get "):  # get data from server
         filename = command[4:]
         data = client.recv(1024)
 
@@ -20,12 +21,24 @@ def executeCommands(client):
             with open(filename, "wb") as file:
                 file.write(data)
 
-    elif command.startswith("put "): # put data from client to server
+    elif command.startswith("put "):  # put data from client to server
         filename = command[4:]
-        with open(filename, "rb") as file:
-            data = file.read()
-            client.send(data) # client send data to server
-            print(client.recv(1024).decode()) # client recovers response from server
+
+        # File does not exist in client
+        if not os.path.exists(filename):
+            client.send(b"FAILURE")
+            print(client.recv(1024).decode())
+        else:
+            with open(filename, "rb") as file:
+                data = file.read()
+                if data:
+                    client.send(b"SUCCESS")
+                    client.send(data)  # client send data to server
+                else:
+                    client.send(b"FAILURE")
+                print(
+                    client.recv(1024).decode()
+                )  # client recovers response from server
     elif command == "ls":
         file_list = client.recv(1024).decode()
         print(file_list)
@@ -50,7 +63,7 @@ def main():
     except InterruptedError:
         print(InterruptedError)
     else:
-        # Server initial response should be 
+        # Server initial response should be
         # "Welcome to the FTP server. Available commands: ls, get <filename>, put <filename>, quit"
         response = client.recv(1024).decode()
         print(response)
@@ -59,6 +72,7 @@ def main():
         run = executeCommands(client)
 
     client.close()
+
 
 if __name__ == "__main__":
     main()
